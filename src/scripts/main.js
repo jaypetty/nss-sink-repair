2fdeb377f325eea86f9e6ebdb4435be13d3bd9bd
jaypetty <jayshaun2343@gmail.com>
2022-01-03 09:35:31 -0600

    working sink repair

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+import { fetchRequests } from "./dataAccess.js"
+import { SinkRepair } from "./SinkRepair.js"
+
+
+const mainContainer = document.querySelector("#container")
+
+const render = () => {
+    fetchRequests().then(
+        () => {
+            mainContainer.innerHTML = SinkRepair()
+        }
+    )
+}
+
+render()
+
+
+mainContainer.addEventListener(
+    "stateChanged",
+    CustomEvent => {
+        render()
+    }
+)
+
+
